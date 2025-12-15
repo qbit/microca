dadcc0de2e45c0dcac44014856feeb8b45c3c760
Aaron Bieber <aaron@bolddaemon.com>
2025-12-15 13:35:29 -0700

    flake: modernize

--- flake.nix
+++ flake.nix
@@ -1,23 +1,30 @@
 {
-  description =
-    "microca: microca is a small, simple Certificate Authority tool.";
+  description = "microca: microca is a small, simple Certificate Authority tool.";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      lastModifiedDate =
-        self.lastModifiedDate or self.lastModified or "19700101";
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
       version = builtins.substring 0 8 lastModifiedDate;
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in {
-      overlay = import ./overlay.nix { inherit self; };
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+    in
+    {
+      overlays.default = import ./overlay.nix { inherit self; };
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           microca = pkgs.buildGoModule {
             pname = "microca";
             inherit version;
@@ -25,18 +32,27 @@
 
             vendorHash = null;
           };
-        });
+        }
+      );
 
-      defaultPackage = forAllSystems (system: self.packages.${system}.microca);
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           default = pkgs.mkShell {
             shellHook = ''
               PS1='\u@\h:\@; '
             '';
-            nativeBuildInputs = with pkgs; [ git go gopls go-tools ];
+            nativeBuildInputs = with pkgs; [
+              git
+              go
+              gopls
+              go-tools
+            ];
           };
-        });
+        }
+      );
     };
 }
